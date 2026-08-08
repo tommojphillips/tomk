b11e31bca098ff0cdb63395a39fde8b2fc215f92
Tommo J. Armytage <tommojphillips@gmail.com>
2026-08-08 19:03:34 +1000

    # Implemented paging_loc_pde and paging_loc_pte routines. # Refactored paging_init routine. # Refactored _map_page routine to use paging_loc_pde and paging_loc_pte routines. in paging.asm.

--- src/kernel/include/paging.h
+++ src/kernel/include/paging.h
@@ -19,6 +19,37 @@
 #define PTE_A  0x20 /* Accessed bit */
 #define PTE_D  0x40 /* Dirty bit */
 
+/* Page Table Entry struct */
+typedef struct pte_t {
+    union {
+        uint32_t dword;
+        struct {
+            uint32_t present    : 1; /* Present */
+            uint32_t rw         : 1; /* 0 = read only; 1 = read and write */
+            uint32_t us         : 1; /* 0 = supervisor; 1 = user */
+            uint32_t r3         : 1; 
+            uint32_t r4         : 1;
+            uint32_t accessed   : 1; /* Accessed */
+            uint32_t dirty      : 1; /* 0 = page is unmodified; 1 = page is dirty (only valid in PTE) */
+            uint32_t r7         : 1;
+            uint32_t r8         : 1;
+            uint32_t available  : 3; /* Available for systems programmer use */
+            uint32_t page_frame : 20;
+        };
+    };
+} pte_t;
+
+/* Page Directory Entry struct */
+typedef pte_t pde_t;
+
+/* Locate PDE containing linear address.
+ Returns a pointer to the page directory entry  */
+extern pde_t* paging_loc_pde(uint32_t pd_base, uint32_t linear_addr);
+
+/* Locate PTE containing linear address.
+ Returns a pointer to the page table entry */
+extern pte_t* paging_loc_pte(uint32_t pd_base, uint32_t linear_addr);
+
 /* Map contiguous pages
 pd_base:              page directory address
 linear_address:       linear start address
--- src/kernel/paging.asm
+++ src/kernel/paging.asm
@@ -28,15 +28,10 @@ paging_init:
 
     mov edx, [esp+8+4]         ; pd_base
     
-    cld                        ; zero PD (PD_BASE, 0, 4096);
+    cld                        ; zero PD/PT (PD_BASE, 0, 4096+(4096*1024));
     xor eax, eax
-    mov ecx, PAGE_SIZE/4
+    mov ecx, (PAGE_SIZE+(PAGE_SIZE*PD_COUNT))/4
     mov edi, edx
-    rep stosd
-    
-    mov ecx, (PAGE_SIZE*PD_COUNT)/4 ; zero PT (PT_BASE, 0, 4096*1024);
-    mov edi, edx
-    add edi, PAGE_SIZE
     rep stosd
 
     mov cr3, edx               ; load page directory address into cr3    
@@ -68,106 +63,89 @@ paging_disable:
     ret
 
 ; map single page
-; edx = pd_base
-; esi = physical_address
-; edi = linear_address
-; eax = flags (lower 12bits)
+; esp+4  = pd_base
+; esp+8  = linear_address
+; esp+12 = physical_address
+; esp+16 = flags (lower 12bits)
 _map_page:
+    push ebx
     push edx
     push esi
     push edi
-    push ebx
 
-    mov eax, [esp+16+4]
-    mov edi, [esp+16+8]
-    mov esi, [esp+16+12]
-    mov edx, [esp+16+16]
+    mov edx, [esp+16+4]
+    mov esi, [esp+16+8]
+    mov edi, [esp+16+12]
+    mov ebx, [esp+16+16]
 
-    ; compute pd_index
-    mov ebx, edi               ; linear_address >> 22
-    shr ebx, 22
-    
-    ; compute pde_address
-    mov esi, edx               ; PDBR 
-    and esi, 0xFFFFF000        ; PDBR clear lower 12bits
-    lea esi, [esi+ebx*4]
+.loc_pde:
+    push esi
+    push edx
+    call paging_loc_pde
+    add esp, 8
 
-    ; compute pt_index
-    mov edx, edi               ; (linear_address >> 12) & 1023
-    shr edx, 12
-    and edx, 0x3FF
-    
-    ; compute pte_address
-    mov edi, ebx               ; (pd_index * 4096) + pd_base + pd_size 
-    shl edi, 12
-    add edi, [esp+16+16]
-    add edi, PD_SIZE
+    ;test dword [eax], P
+    ;jnz .loc_pte
 
-    mov eax, [esi]             ; get pde
-    test eax, P                ; pde present?
-    jnz .build_pte             ; yes, skip pde
+.build_pde:
+    mov ecx, esi               ; pd_index = linear_address >> 22
+    shr ecx, 22
+    shl ecx, 12
+    add ecx, edx
+    add ecx, PD_SIZE
+    or ecx, US | RW | P        ; set flags
+    mov [eax], ecx             ; write pde
 
-.build_pde:                    ; no, build pde
-    mov eax, edi               ; pte address
-    or eax, US | RW | P        ; set present bit
-    mov [esi], eax             ; set pde
+.loc_pte:
+    push esi
+    push edx
+    call paging_loc_pte
+    add esp, 8
 
 .build_pte:                    ; build pte
-    mov esi, [esp+12+16]       ; get physical_address
-    and esi, 0xFFFFF000        ; clear lower 12bits    
-    mov eax, [esp+12+8]        ; get flags
-    and eax, 0x00000FFF        ; clear upper 20bits
-    or eax, esi                ; set upper 20bits to physical_frame_address
-    mov [edi+edx*4], eax       ; copy pte to memory 
+    mov ecx, edi               ; get physical_address
+    and ecx, 0xFFFFF000        ; clear lower 12bits
+    and ebx, 0x00000FFF        ; clear upper 20bits in flags
+    or ecx, ebx                ; set upper 20bits to physical_frame_address
+    mov [eax], ecx             ; copy pte to memory 
 
 .done:
-    pop ebx
     pop edi
     pop esi
     pop edx
+    pop ebx
     ret
 
 ; map contiguous pages
-; edx = pd_base
-; esi = start linear_address
-; edi = start physical_address
-; eax = flags (lower 12bits)
-; ecx = count (in 4096-byte units)
+; esp+4  = pd_base
+; esp+8  = linear_address
+; esp+12 = physical_address
+; esp+16 = flags (lower 12bits)
+; esp+20 = count (in 4096-byte units)
 paging_map:
-    push edi
-    push esi
-    push edx
-    push ebx
+    push ebp                           ; save ebp
+    mov ebp, esp                       ; save frame ptr
+    add ebp, 4                         ; point frame ptr at params-4
 
-    mov ecx, [esp+16+4]                ; page count (in 4096-byte units)
-    mov eax, [esp+16+8]                ; flags (lower 12bits)
-    mov edi, [esp+16+12]               ; start physical_address
-    mov esi, [esp+16+16]               ; start linear_address
-    mov edx, [esp+16+20]               ; pd_base
-
-    test ecx, ecx                      ; count == zero?
+    cmp dword [ebp+20], 0              ; count == zero?
     jz .done                           ; yes, dont map anything.
 
 .nxt:
-    push ecx                           ; save page count
-    push edx
-    push esi
-    push edi
-    push eax
+    push dword [ebp+16]                ; flags (lower 12bits)
+    push dword [ebp+12]                ; physical_address
+    push dword [ebp+8]                 ; linear_address
+    push dword [ebp+4]                 ; pd_base
     call _map_page
     add esp, 16
-    pop ecx                           ; restore page count
+
+    add dword [ebp+8], PAGE_SIZE       ; linear_address += 4096
+    add dword [ebp+12], PAGE_SIZE      ; physical_address += 4096
+    dec dword [ebp+20]
     
-    add esi, PAGE_SIZE                ; linear_address += 4096
-    add edi, PAGE_SIZE                ; physical_address += 4096
-    dec ecx
     jnz .nxt
 
 .done:
-    pop ebx
-    pop edx
-    pop esi
-    pop edi
+    pop ebp                            ; restore ebp
     ret
 
 ; flush TLB
@@ -175,4 +153,54 @@ paging_map:
 paging_flush:
     mov eax, cr3
     mov cr3, eax
+    ret
+
+; located PDE
+; Returns pointer to PDE
+paging_loc_pde:
+    push edx
+    push edi
+
+    mov edx, [esp+8+4]         ; pd_base
+    mov edi, [esp+8+8]         ; linear_address
+
+    ; compute pd_index
+    mov eax, edi               ; linear_address >> 22
+    shr eax, 22
+    
+    ; compute pde_address
+    mov ecx, edx
+    and ecx, 0xFFFFF000        ; pd_base clear lower 12bits
+   
+    lea eax, [ecx+eax*4]       ; pd_base + pd_index * 4
+
+    pop edi
+    pop edx
+    ret
+
+; located PTE
+; Returns pointer to PTE
+paging_loc_pte:
+    push edx
+    push edi
+
+    mov edx, [esp+8+4]         ; pd_base
+    mov edi, [esp+8+8]         ; linear_address
+
+    ; compute pt_index
+    mov eax, edi
+    shr eax, 12
+    and eax, 0x3FF             ; pt_index = (linear_address >> 12) & 0x3FF
+
+    ; compute pt_base
+    mov ecx, edi
+    shr ecx, 22
+    shl ecx, 12
+    add ecx, edx
+    add ecx, PD_SIZE
+
+    lea eax, [ecx+eax*4]       ; pt_base + pt_index * 4
+
+    pop edi
+    pop edx
     ret
