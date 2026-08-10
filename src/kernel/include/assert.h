05fcef26a48dcbd503bbfc898bd0286ae0b252ed
Tommo J. Armytage <tommojphillips@gmail.com>
2026-08-10 16:12:31 +1000

    # Added assert.h

---
+++ src/kernel/include/assert.h
@@ -0,0 +1,13 @@
+#ifndef _ASSERT_H
+#define _ASSERT_H
+
+#include <kernel.h>
+
+#define ASSERT_ENABLE
+#ifdef ASSERT_ENABLE
+#define assert(x, ...) do { if (!(x)) { kernel_panic(__VA_ARGS__); } } while(0);
+#else
+#define assert(x, ...)
+#endif
+
+#endif
