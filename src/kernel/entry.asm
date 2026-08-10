930543293e694adda353fa386d69e55a5d7a1df6
Tommo J. Armytage <tommojphillips@gmail.com>
2026-08-10 16:11:22 +1000

    # Removed kernel_hang function from entry.asm # Added CLI instruction to entry to ensure interrupts are disabled.

--- src/kernel/entry.asm
+++ src/kernel/entry.asm
@@ -1,12 +1,10 @@
 ; entry.asm
 BITS 32
 
-extern printf                ; libc\stdio\printf.c
 extern kernel_main           ; kernel.c
 
 global multiboot_info_ptr
 global kernel_entry
-global kernel_hang
 
 section .bss
 stack_bottom:
@@ -14,15 +12,14 @@ stack_bottom:
     stack_buf resb 16*1024   ; reserve 16kb for stack
 stack_top:
 
-section .rodata
-    hang_str db "HANG @ %8.8X", 0
-
 section .data
     multiboot_info_ptr dd 0    
 
 section .text
 
 kernel_entry:
+    cli
+    
     cmp eax, 0x2BADB002      ; multiboot ?
     jnz .unk                 ; no
 
@@ -36,10 +33,6 @@ kernel_entry:
     mov esp, stack_top       ; change stacks
     call kernel_main         ; call into the kernel
 
-kernel_hang:
-    push hang_str
-    call printf
-    add esp, 4
 .hang:
     cli
     hlt
