be313eadd34dbdadbe73a97c4fc7fd951df8b2f6
Tommo J. Armytage <tommojphillips@gmail.com>
2026-08-16 16:18:19 +1000

    # Added kprint macro to kernel.h

--- src/kernel/include/kernel.h
+++ src/kernel/include/kernel.h
@@ -5,6 +5,14 @@
 #ifndef KERNEL_H
 #define KERNEL_H
 
+#define KDBG
+#ifdef KDBG
+#include <stdio.h>
+#define kprint(...) printf(__VA_ARGS__)
+#else
+#define kprint(...)
+#endif
+
 void kernel_panic(const char* msg);
 void kernel_hang(void);
 
