44c6e86e2b528f68fdbfbeeec08e354305b8984c
Tommo J. Armytage <tommojphillips@gmail.com>
2026-08-16 16:23:03 +1000

    # Added asserts to kmmap functions in kmmap.c

--- src/kernel/kmmap.c
+++ src/kernel/kmmap.c
@@ -4,31 +4,30 @@
 
 #include <stdint.h>
 #include <stddef.h>
-#include <stdio.h>
 
-#include <assert.h>
-#include <kernel.h>
-#include <kalloc.h>
 #include <kmmap.h>
+#include <kernel.h>
+#include <assert.h>
+#include <kalloc.h>
 
 void kmmap_init(kmmap_t** kmmap, size_t size) {
-	assert(kmmap != NULL, "kmmap NULL\n");
+	assert(kmmap != NULL);
 	*kmmap = kalloc(size);	
-	assert(*kmmap != NULL, "kmmap alloc failed\n");
+	assert(*kmmap != NULL);
 	(*kmmap)->count = 0;
 	(*kmmap)->capacity = (size - sizeof(kmmap_t)) / sizeof(kmregion_t);
 	(*kmmap)->regions = (kmregion_t*)((char*)*kmmap + sizeof(kmmap_t));
 }
 void kmmap_add(kmmap_t* kmmap, uint64_t address, uint64_t size, uint32_t flags) {
-	assert(kmmap->count < kmmap->capacity, "kmmap at capacity\n");
-	assert(kmmap != NULL, "kmmap NULL\n");
+	assert(kmmap != NULL);
+	assert(kmmap->count < kmmap->capacity);
 	kmmap->regions[kmmap->count].address = address;
 	kmmap->regions[kmmap->count].size = size;
 	kmmap->regions[kmmap->count].flags = flags | KMREGION_VALID;
 	kmmap->count++;
 }
 void kmmap_remove(kmmap_t* kmmap, uint64_t address) {
-	assert(kmmap != NULL, "kmmap NULL\n");
+	assert(kmmap != NULL);
 	for (size_t i = 0; i < kmmap->count; ++i) {
 		if (kmmap->regions[i].address == address) {			
 			kmmap->regions[i].address = 0;
