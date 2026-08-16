a571a3d442c6270e6b3b85f1bd1a930af4ccb9fe
Tommo J. Armytage <tommojphillips@gmail.com>
2026-08-16 16:20:27 +1000

    # Added asserts to mb_init

--- src/kernel/mb.c
+++ src/kernel/mb.c
@@ -4,33 +4,30 @@
 
 #include <stdint.h>
 #include <stddef.h>
-#include <stdio.h>
 
 #include <mb.h>
 #include <kmmap.h>
 #include <kernel.h>
+#include <assert.h>
 
 extern multiboot_info_t* multiboot_info_ptr; /* entry.asm */
 
 void mb_init(kmmap_t* kmmap) {
-	if (multiboot_info_ptr->flags & MULTIBOOT_FLAGS_MMAP) {
-		multiboot_mmap_t* mmap = (multiboot_mmap_t*)multiboot_info_ptr->mmap_addr;
+	assert(multiboot_info_ptr != NULL);
+	assert(multiboot_info_ptr->flags & MULTIBOOT_FLAGS_MMAP);
+
+	multiboot_mmap_t* mmap = (multiboot_mmap_t*)multiboot_info_ptr->mmap_addr;
+	while ((uintptr_t)mmap < multiboot_info_ptr->mmap_addr + multiboot_info_ptr->mmap_length) {
+		uint64_t addr = ((uint64_t)mmap->addr2 << 32) |  (uint64_t)mmap->addr1;
+		uint64_t len = ((uint64_t)mmap->len2 << 32) |  (uint64_t)mmap->len1;
 		
-		while ((uintptr_t)mmap < multiboot_info_ptr->mmap_addr + multiboot_info_ptr->mmap_length) {
-			uint64_t addr = ((uint64_t)mmap->addr2 << 32) |  (uint64_t)mmap->addr1;
-			uint64_t len = ((uint64_t)mmap->len2 << 32) |  (uint64_t)mmap->len1;
-			
-			if (mmap->type == MULTIBOOT_MMAP_TYPE_RAM) {
-				kmmap_add(kmmap, addr, len, KMREGION_TYPE_RAM);
-			}
-			else {
-				kmmap_add(kmmap, addr, len, KMREGION_TYPE_REV);
-			}
-			
-			mmap = (multiboot_mmap_t*)((uintptr_t)mmap + mmap->size + sizeof(mmap->size));
+		if (mmap->type == MULTIBOOT_MMAP_TYPE_RAM) {
+			kmmap_add(kmmap, addr, len, KMREGION_TYPE_RAM);
 		}
+		else {
+			kmmap_add(kmmap, addr, len, KMREGION_TYPE_REV);
+		}
+		
+		mmap = (multiboot_mmap_t*)((uintptr_t)mmap + mmap->size + sizeof(mmap->size));
 	}
-    else {
-        kernel_panic("ERROR: Multiboot mmap not present\n");
-    }
 }
