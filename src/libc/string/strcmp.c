d5ee97e05ed9c3711dc8c6392a65494642563f31
Tommo J. Armytage <tommojphillips@gmail.com>
2026-08-16 17:12:30 +1000

    # Fixed strcmp and strncmp to return -1, 0, 1

--- src/libc/string/strcmp.c
+++ src/libc/string/strcmp.c
@@ -4,29 +4,32 @@
 #include <string.h>
 
 int strcmp(const char* restrict s1, const char* restrict s2) {
-    size_t l1 = strlen(s1);
-    size_t l2 = strlen(s2);
+    for (;;) {
+        unsigned char c1 = (unsigned char)*s1++;
+        unsigned char c2 = (unsigned char)*s2++;
 
-    if (l1 != l2) {
-        return 1;
-    }
-
-	for (size_t i = 0; i < l1; ++i) {
-		if (s1[i] != s2[i]) {
-            return 1;
+        if (c1 != c2) {
+            return c1 < c2 ? -1 : 1;
         }
-	}
-	return 0;
+
+        if (c1 == '\0') {
+            return 0;
+        }
+    }
 }
 int strncmp(const char* restrict s1, const char* restrict s2, size_t size) {
-    size_t l1 = strlen(s1);
-    size_t l2 = strlen(s2);
-    size_t l = l1 < l2 ? l1 : l2 < size ? l2 : size;
+    for (size_t i = 0; i < size; ++i) {
+        unsigned char c1 = (unsigned char)s1[i];
+        unsigned char c2 = (unsigned char)s2[i];
 
-	for (size_t i = 0; i < l; ++i) {
-		if (s1[i] != s2[i]) {
-            return 1;
+        if (c1 != c2) {
+            return c1 < c2 ? -1 : 1;
         }
-	}
-	return 0;
+
+        if (c1 == '\0') {
+            break;
+        }
+    }
+
+    return 0;
 }
