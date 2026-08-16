c3b6a60319979d95feeff2dcdaba07ee8ba2e3b5
Tommo J. Armytage <tommojphillips@gmail.com>
2026-08-16 16:16:34 +1000

    # Modified assert macro

--- src/kernel/include/assert.h
+++ src/kernel/include/assert.h
@@ -12,7 +12,7 @@
 #ifdef ASSERT_ENABLE
 #define STRINGIFY_(x) #x
 #define STRINGIFY(x) STRINGIFY_(x)
-#define assert(x, s) do { if (!(x)) { printf("ASSERT: \""#x"\" FAILED.\n in " __FILE__ " Ln " STRINGIFY(__LINE__) "\n"); kernel_panic(s); } } while(0)
+#define assert(x) do { if (!(x)) { kernel_panic("ASSERT: \""#x"\" FAILED.\n in " __FILE__ " Ln " STRINGIFY(__LINE__) "\n"); } } while(0)
 
 #else
 #define assert(x, ...)
