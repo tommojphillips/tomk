d67f5d8832f15a7dc18b081f996c0bba1eebe0bb
Tommo J. Armytage <tommojphillips@gmail.com>
2026-08-16 16:28:07 +1000

    # Added dbg mode to build.bat that complies with debug symbols creates a symbol file then strips the debug symbols from the elf. # Added disasm directory to assemble/compile list

--- build.bat
+++ build.bat
@@ -5,7 +5,9 @@ set "out_dir=bin\%out_fn%\"
 set "obj_dir=obj\%out_fn%\"
 set "link_file=src\%out_fn%\linker.ld"
 
-set "c_includes=-I "src\kernel\include" -I "src\driver\include" -I "src\libc\include""
+set "dbg=1"
+
+set "c_includes=-I "src\kernel\include" -I "src\driver\include" -I "src\libc\include" -I "src\disasm""
 set "c_arguments=-std=gnu99 -ffreestanding -O2 -Wall -Wextra -march=i386"
 set "asm_args=-f elf32"
 set "obj_str="
@@ -17,6 +19,11 @@ if not exist "%out_dir%" (
 
 if not exist "%obj_dir%" (
  mkdir "%obj_dir%"
+)
+
+if %dbg%  == 1 (
+    set "c_arguments=%c_arguments% -g"
+    set "asm_args=%asm_args% -g"
 )
 
 cls
@@ -39,6 +46,10 @@ REM KERNEL
 call :assemble_dir "src\kernel\" "obj\kernel\" || goto :exit__
 call :compile_dir "src\kernel\" "obj\kernel\" || goto :exit__
 
+REM DISASM
+call :assemble_dir "src\disasm\" "obj\disasm\" || goto :exit__
+call :compile_dir "src\disasm\" "obj\disasm\" || goto :exit__
+
 echo.
 echo Linking...
 
@@ -47,8 +58,24 @@ if not exist "%link_file%" (
  goto :exit__
 )
 
-ld %link_args% "-Map=%out_dir%%out_fn%.map" -T "%link_file%" -o "%out_dir%%out_fn%.elf" %obj_str% || goto :exit__
-echo out -^>^ %out_dir%%out_fn%.elf
+i686-elf-ld %link_args% "-Map=%out_dir%%out_fn%.map" -T "%link_file%" -o "%out_dir%%out_fn%.elf" %obj_str% || goto :exit__
+
+echo Decompiling..
+i686-elf-objdump -m i386 -M intel -d %out_dir%%out_fn%.elf > %out_dir%%out_fn%.asm || goto :exit__
+ 
+if %dbg% == 1 (
+ echo Creating symbol file...
+ objcopy --only-keep-debug %out_dir%%out_fn%.elf %out_dir%%out_fn%.sym || goto :exit__
+ 
+ echo Stripping...
+ objcopy --strip-debug %out_dir%%out_fn%.elf
+ 
+ echo out -^>^ %out_dir%%out_fn%.sym
+ echo out -^>^ %out_dir%%out_fn%.elf
+) else (
+ echo out -^>^ %out_dir%%out_fn%.elf
+)
+echo out -^>^ %out_dir%%out_fn%.asm
 
 exit /b 0
 
@@ -72,7 +99,7 @@ exit /b 0
   goto :exit__
  )
  i686-elf-gcc -c "%~1%~2" -o "%obj_dir%%~2.o" %c_includes% %~3 %c_arguments% || goto :exit__
- set "obj_str=%obj_str%%obj_dir%%~2.o "
+ set "obj_str=%obj_dir%%~2.o %obj_str%"
  setlocal EnableDelayedExpansion
  set "SRC=%~1%~2"
  set "OUT=%obj_dir%%~2.o"
