6c509b5e05f1e77c2743af48c1134d3b5a3bb19f
Tommo J. Armytage <tommojphillips@gmail.com>
2026-08-31 17:37:54 +1000

    # Changed .multiboot section name to .mb

--- src/boot/multiboot.asm
+++ src/boot/multiboot.asm
@@ -4,8 +4,6 @@
 ; Multiboot header
 ;
 
-BITS 32
-
 MB_MAGIC    equ 0x1BADB002
 MB_4KB      equ (1 << 0)
 MB_MMAP     equ (1 << 1)
@@ -13,7 +11,7 @@ MB_MMAP     equ (1 << 1)
 MB_FLAGS    equ (MB_4KB | MB_MMAP)
 MB_CHECKSUM equ (0 - (MB_MAGIC + MB_FLAGS))
  
-section .multiboot
+section .mb
 	align 4, db 0
 	dd MB_MAGIC
 	dd MB_FLAGS
