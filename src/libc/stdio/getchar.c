76690e19a29e4714c295e7aa209e31b1cb30c2ee
Tommo J. Armytage <tommojphillips@gmail.com>
2026-09-06 08:47:45 +1000

    # getchar() now read from serial port aswell as ps/2 keyboard

--- src/libc/stdio/getchar.c
+++ src/libc/stdio/getchar.c
@@ -2,11 +2,19 @@
 
 #include <stdio.h>
 #include <ps2.h>
+#include <uart.h>
 
 int getchar(void) {
+#ifdef LIBK
     int ch = ps2_getchar();
+    if (ch == 0) {
+        ch = serial_read();
+    }    
     if (ch == 0) {
         return EOF;
     }
     return ch;
+#else
+    return 0;
+#endif
 }
